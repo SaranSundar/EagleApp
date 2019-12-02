0151e2d30f5a06aa332c29c38449f6c49f1eebbd
Ken Asawa <kxa161130@utdallas.edu>
2019-12-01 19:42:37 -0600

    Added camera page functionality

--- lib/camera_page.dart
+++ lib/camera_page.dart
@@ -1,10 +1,3 @@
 import 'package:flutter/material.dart';
 
-class CameraPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return new Scaffold(
-      backgroundColor: Colors.cyanAccent[700],
-    );
-  }
-}
+//Depreciated file. Delete this later.
--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,20 @@
-import 'package:eagle/loc_with_speech.dart';
-import 'package:eagle/speak_text.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:eagle/loc_with_speech.dart';
+import 'package:eagle/speak_text.dart';
+import 'dart:async';
+import 'package:camera/camera.dart';
 import 'contact_page.dart';
 import 'locations_page.dart';
 import 'maps_page.dart';
-import 'camera_page.dart';
 
 final controller = PageController(initialPage: 1);
+List<CameraDescription> cameras;
 
-void main() => runApp(MyApp());
+Future<void> main() async {
+  cameras = await availableCameras();
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
@@ -49,9 +54,47 @@ class _MyHomePageState extends State<MyHomePage> {
           LocWithSpeech(),
           LocationsPage(controller: controller),
           MapsPage(),
-          CameraPage()
+          CameraApp()
         ],
       ),
     );
+  }
+}
+
+class CameraApp extends StatefulWidget {
+  @override
+  CameraAppState createState() => CameraAppState();
+}
+
+class CameraAppState extends State<CameraApp> {
+  CameraController controller;
+  @override
+  void initState() {
+    super.initState();
+    controller = CameraController(cameras[0], ResolutionPreset.medium);
+    controller.initialize().then((_) {
+      if (!mounted) {
+        return;
+      }
+
+      setState(() {});
+    });
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (!controller.value.isInitialized) {
+      return Container();
+    }
+    return AspectRatio(
+        aspectRatio:
+        controller.value.aspectRatio,
+        child: CameraPreview(controller));
   }
 }
