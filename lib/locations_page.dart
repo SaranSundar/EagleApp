3524a0fb180b1faf664c8f945aed59ad6e0c2a9f
Saran Sundararajan <saransszg@gmail.com>
2019-12-01 22:30:52 -0600

    Working?

--- lib/locations_page.dart
+++ lib/locations_page.dart
@@ -1,73 +1,141 @@
+import 'package:eagle/ReadText.dart';
 import 'package:flutter/material.dart';
 
-class LocationsPage extends StatelessWidget{
+class LocationsPage extends StatefulWidget {
   final PageController controller;
-  static const _kDuration = const Duration(milliseconds: 300);
-  static const _kCurve = Curves.ease;
-  var roomNames = ["Classroom A", "Classroom B", "Classroom C", "Classroom D"];
-  var roomLocations = ["ECSS Floor 2, Room 2.12", "ECSN Floor 3, Room 3.41", "CB Floor 1, Room 1.04", "ECSW Floor 1, Room 1.18"];
-  var roomImages = ['assets/images/classroom1.jpg', 'assets/images/classroom2.jpg', 'assets/images/classroom3.jpg', 'assets/images/classroom4.jpg'];
 
   LocationsPage({Key key, @required this.controller}) : super(key: key);
 
   @override
+  _LocationsPageState createState() => _LocationsPageState(controller);
+}
+
+class _LocationsPageState extends State<LocationsPage> {
+  static const _kDuration = const Duration(milliseconds: 300);
+  static const _kCurve = Curves.ease;
+  final PageController controller;
+  var roomNames = ["Classroom A", "Classroom B", "Classroom C", "Classroom D"];
+  var roomLocations = [
+    "ECSS Floor 2, Room 2.12",
+    "ECSN Floor 3, Room 3.41",
+    "CB Floor 1, Room 1.04",
+    "ECSW Floor 1, Room 1.18"
+  ];
+  var roomImages = [
+    'assets/images/classroom1.jpg',
+    'assets/images/classroom2.jpg',
+    'assets/images/classroom3.jpg',
+    'assets/images/classroom4.jpg'
+  ];
+  var readText = ReadText();
+
+  _LocationsPageState(this.controller);
+
+  @override
+  initState() {
+    super.initState();
+    readText.initTts();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    readText.flutterTts.stop();
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final _size = MediaQuery.of(context).size;
     return new Scaffold(
-      backgroundColor: Colors.cyanAccent[700],
-      body: ListView.builder(
-          padding: const EdgeInsets.all(4),
-          itemCount: 4,
-          itemBuilder: (BuildContext context, int index) {
-            return new GestureDetector(
-                onTap: () {
-                  controller.animateToPage(2, duration: _kDuration, curve: _kCurve);
-                },
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(30.0),
-                  child: Container(
-                    height: 160,
-                    margin: const EdgeInsets.all(8.0),
-                    color: Colors.white,
-                    child: Column(
-                      children: <Widget>[
-                        Container(
-                          width: MediaQuery.of(context).size.width,
-                          height: 110,
-                          decoration: BoxDecoration(
-                            image: DecorationImage(
-                              fit: BoxFit.cover,
-                              image: AssetImage(roomImages[index]),
-                            ),
-                          ),
-                        ),
-                        Align(
-                          alignment: Alignment.centerLeft,
-                          child: Container(
-                            margin: const EdgeInsets.symmetric(horizontal: 8.0),
-                            child: Text(
-                                roomNames[index],
-                                style: TextStyle(
-                                    fontSize: 26
-                                )
-                            ),
-                          ),
-                        ),
-                        Align(
-                          alignment: Alignment.centerLeft,
-                          child: Container(
-                            margin: const EdgeInsets.symmetric(horizontal: 8.0),
-                            child: Text(
-                              "Located in " + roomLocations[index],
-                            ),
-                          ),
-                        ),
-                      ]
-                    )
+        backgroundColor: Colors.cyanAccent[700],
+        body: Column(
+          children: <Widget>[
+            Expanded(
+              child: Container(
+                child: ListView.builder(
+                    padding: const EdgeInsets.all(4),
+                    itemCount: 4,
+                    itemBuilder: (BuildContext context, int index) {
+                      return new GestureDetector(
+                          onTap: () {
+                            controller.animateToPage(2,
+                                duration: _kDuration, curve: _kCurve);
+                          },
+                          child: ClipRRect(
+                              borderRadius: BorderRadius.circular(30.0),
+                              child: Container(
+                                  height: 160,
+                                  margin: const EdgeInsets.all(8.0),
+                                  color: Colors.white,
+                                  child: Column(children: <Widget>[
+                                    Container(
+                                      width: MediaQuery.of(context).size.width,
+                                      height: 110,
+                                      decoration: BoxDecoration(
+                                        image: DecorationImage(
+                                          fit: BoxFit.cover,
+                                          image: AssetImage(roomImages[index]),
+                                        ),
+                                      ),
+                                    ),
+                                    Align(
+                                      alignment: Alignment.centerLeft,
+                                      child: Container(
+                                        margin: const EdgeInsets.symmetric(
+                                            horizontal: 8.0),
+                                        child: Text(roomNames[index],
+                                            style: TextStyle(fontSize: 26)),
+                                      ),
+                                    ),
+                                    Align(
+                                      alignment: Alignment.centerLeft,
+                                      child: Container(
+                                        margin: const EdgeInsets.symmetric(
+                                            horizontal: 8.0),
+                                        child: Text(
+                                          "Located in " + roomLocations[index],
+                                        ),
+                                      ),
+                                    ),
+                                  ]))));
+                    }),
+              ),
+            ),
+            Container(
+              width: double.infinity,
+              // color: Colors.blue,
+              height: _size.height * 0.25,
+              // Take 25% width of the screen height
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  SizedBox(
+                    width: _size.width * 0.5,
+                    height: double.infinity,
+                    child: RaisedButton(
+                      child: Text('Speech to text'),
+                      onPressed: () {
+                        print("Speech to text");
+                      },
+                    ),
+                  ),
+                  SizedBox(
+                    width: _size.width * 0.5,
+                    height: double.infinity,
+                    child: RaisedButton(
+                      child: Text('Text to speech'),
+                      onPressed: () {
+                        print("Text to speech");
+                        readText.speak();
+                        setState(() {});
+                      },
+                    ),
                   )
-                )
-            );
-          }
-      )
-    );
+                ],
+              ),
+            )
+          ],
+        ));
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,15 @@
+import 'camera_page.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:eagle/loc_with_speech.dart';
-import 'package:eagle/speak_text.dart';
-import 'dart:async';
-import 'package:camera/camera.dart';
+import 'loc_with_speech.dart';
+import 'speak_text.dart';
 import 'contact_page.dart';
 import 'locations_page.dart';
 import 'maps_page.dart';
 
 final controller = PageController(initialPage: 1);
-List<CameraDescription> cameras;
 
-Future<void> main() async {
-  cameras = await availableCameras();
+void main() {
   runApp(MyApp());
 }
 
@@ -54,47 +51,9 @@ class _MyHomePageState extends State<MyHomePage> {
           LocWithSpeech(),
           LocationsPage(controller: controller),
           MapsPage(),
-          CameraApp()
+          CameraApp(),
         ],
       ),
     );
-  }
-}
-
-class CameraApp extends StatefulWidget {
-  @override
-  CameraAppState createState() => CameraAppState();
-}
-
-class CameraAppState extends State<CameraApp> {
-  CameraController controller;
-  @override
-  void initState() {
-    super.initState();
-    controller = CameraController(cameras[0], ResolutionPreset.medium);
-    controller.initialize().then((_) {
-      if (!mounted) {
-        return;
-      }
-
-      setState(() {});
-    });
-  }
-
-  @override
-  void dispose() {
-    controller.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    if (!controller.value.isInitialized) {
-      return Container();
-    }
-    return AspectRatio(
-        aspectRatio:
-        controller.value.aspectRatio,
-        child: CameraPreview(controller));
   }
 }
