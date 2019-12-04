8b30b4de9c14525625a50fe841e795b1adc55e7a
Saran Sundararajan <saransszg@gmail.com>
2019-12-04 16:44:16 -0600

    Phone directly

--- lib/contact_page.dart
+++ lib/contact_page.dart
@@ -1,26 +1,70 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'ReadText.dart';
 import 'SpeakText.dart';
 
 class ContactPage extends StatefulWidget {
   final PageController controller;
+
   ContactPage({Key key, @required this.controller}) : super(key: key);
+
   @override
   _ContactPageState createState() => _ContactPageState(controller);
 }
 
 class _ContactPageState extends State<ContactPage> {
   final PageController controller;
+  static const platform = const MethodChannel('samples.flutter.dev/battery');
+  static const phoneCall = const MethodChannel('samples.flutter.dev/phoneCall');
+
+  // Get battery level.
+  String _batteryLevel = 'Unknown battery level.';
+
   final List<String> contacts = [
-    'Katie','Ken','Mark','Nikhil','Ricardo','Saran'
+    'Ken',
+    'Nikhil',
+    'Saran'
   ];
   var readText = ReadText();
   var speakText = SpeakText();
   var contactsText = "You have 6 available contacts." +
-      "Katie." + "Ken." + "Mark." + "Nikhil." + "Ricardo" +
-      " and." + "Saran." + "Who would you like to call?";
+      "Katie." +
+      "Ken." +
+      "Mark." +
+      "Nikhil." +
+      "Ricardo" +
+      " and." +
+      "Saran." +
+      "Who would you like to call?";
+
   _ContactPageState(this.controller);
+
+  Future<void> _makePhoneCall() async {
+    try {
+      final int result = await phoneCall.invokeMethod('makePhoneCall');
+      print(result);
+    } on PlatformException catch (e) {
+      print(e.message);
+    }
+
+    setState(() {
+    });
+  }
+
+  Future<void> _getBatteryLevel() async {
+    String batteryLevel;
+    try {
+      final int result = await platform.invokeMethod('getBatteryLevel');
+      batteryLevel = 'Battery level at $result % .';
+    } on PlatformException catch (e) {
+      batteryLevel = "Failed to get battery level: '${e.message}'.";
+    }
+
+    setState(() {
+      _batteryLevel = batteryLevel;
+    });
+  }
 
   @override
   initState() {
@@ -28,7 +72,7 @@ class _ContactPageState extends State<ContactPage> {
     readText.initTts();
     speakText.initSpeechRecognizer();
     speakText.speechRecognition.setRecognitionCompleteHandler(
-          () => this.printFinalText(),
+      () => this.printFinalText(),
     );
   }
 
@@ -103,66 +147,80 @@ class _ContactPageState extends State<ContactPage> {
     final _size = MediaQuery.of(context).size;
     return new Scaffold(
         backgroundColor: Colors.cyanAccent[700],
-        body: Column(
-            children: <Widget>[
-              Expanded(
-                child: new Container(
-                  child: new ListView.builder(
-                    reverse: false,
-                    itemBuilder: (_,int index)=>EachList(this.contacts[index]),
-                    itemCount: this.contacts.length,
+        body: Column(children: <Widget>[
+          RaisedButton(
+            child: Text('Get Battery Level'),
+            onPressed: _getBatteryLevel,
+          ),
+          Text(_batteryLevel),
+          RaisedButton(
+            child: Text('Make Phone Call'),
+            onPressed: _makePhoneCall,
+          ),
+          Expanded(
+            child: new Container(
+              child: new ListView.builder(
+                reverse: false,
+                itemBuilder: (_, int index) => EachList(this.contacts[index]),
+                itemCount: this.contacts.length,
+              ),
+            ),
+          ),
+          Container(
+            width: double.infinity,
+            // color: Colors.blue,
+            height: _size.height * 0.25,
+            // Take 25% width of the screen height
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                SizedBox(
+                  width: _size.width * 0.5,
+                  height: double.infinity,
+                  child: RaisedButton(
+                    color: Colors.red,
+                    child: Text(
+                      'Voice Commands',
+                      style: TextStyle(fontSize: 20.0),
+                    ),
+                    onPressed: () {
+                      readText.stop();
+                      print("Speech to text");
+                      speakText.speechRecognition
+                          .listen(locale: "en_US")
+                          .then((result) => {});
+                    },
                   ),
                 ),
-              ),
-              Container(
-                width: double.infinity,
-                // color: Colors.blue,
-                height: _size.height * 0.25,
-                // Take 25% width of the screen height
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    SizedBox(
-                      width: _size.width * 0.5,
-                      height: double.infinity,
-                      child: RaisedButton(
-                        color: Colors.red,
-                        child: Text('Voice Commands'),
-                        onPressed: () {
-                          readText.stop();
-                          print("Speech to text");
-                          speakText.speechRecognition
-                              .listen(locale: "en_US")
-                              .then((result) => {});
-                        },
-                      ),
+                SizedBox(
+                  width: _size.width * 0.5,
+                  height: double.infinity,
+                  child: RaisedButton(
+                    color: Colors.green,
+                    child: Text(
+                      'Read Contacts',
+                      style: TextStyle(fontSize: 20.0),
                     ),
-                    SizedBox(
-                      width: _size.width * 0.5,
-                      height: double.infinity,
-                      child: RaisedButton(
-                        color: Colors.green,
-                        child: Text('Read Contacts'),
-                        onPressed: () {
-                          print("Text to speech");
-                          readText.speak(contactsText);
-                          setState(() {});
-                        },
-                      ),
-                    )
-                  ],
-                ),
-              )
-            ]
-        )
-    );
+                    onPressed: () {
+                      print("Text to speech");
+                      readText.speak(contactsText);
+                      setState(() {});
+                    },
+                  ),
+                )
+              ],
+            ),
+          )
+        ]));
   }
 }
 
-class EachList extends StatelessWidget{
+class EachList extends StatelessWidget {
   final String name;
+
   EachList(this.name);
+
   @override
   Widget build(BuildContext context) {
     final _size = MediaQuery.of(context).size;
@@ -175,9 +233,14 @@ class EachList extends StatelessWidget{
           padding: EdgeInsets.all(8.0),
           child: new Row(
             children: <Widget>[
-              new CircleAvatar(child: new Text(name[0]),),
+              new CircleAvatar(
+                child: new Text(name[0]),
+              ),
               new Padding(padding: EdgeInsets.only(right: 10.0)),
-              new Text(name,style: TextStyle(fontSize: 20.0),)
+              new Text(
+                name,
+                style: TextStyle(fontSize: 20.0),
+              )
             ],
           ),
         ),
