132c5a256c7a5dd44fbc802b361217401f99ac7c
Ken Asawa <kxa161130@utdallas.edu>
2019-12-04 22:43:46 -0600

    Detects falling

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,8 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'contact_page.dart';
 import 'locations_page.dart';
 import 'maps_page.dart';
+import 'package:sensors/sensors.dart';
+import 'dart:async';
 
 final controller = PageController(initialPage: 1);
 
@@ -26,6 +28,61 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  AccelerometerEvent event; // Event returned from accelerometer stream
+  Timer timer;
+  StreamSubscription accel;
+  double prevZ = 10;
+  bool alerted = false;
+
+  @override
+  void initState() {
+    super.initState();
+    monitorAcceleration();
+  }
+
+  void monitorAcceleration() {
+    // If the accelerometer subscription hasn't been created, go ahead and create it
+    if (accel == null) {
+      accel = accelerometerEvents.listen((AccelerometerEvent eve) {
+        setState(() {
+          event = eve;
+          // If it detects a rapid change in height, shows an alert message.
+          if((prevZ - event.z).abs() > 5 && !alerted){
+            alerted = true;
+            showAlert(context);
+          }
+          //print("PrevZ: " + prevZ.toString() + "CurZ: " + (event.z).toString());
+          prevZ = event.z;
+        });
+      });
+    } else {
+      // If it has already been created just resume it
+      accel.resume();
+    }
+  }
+
+  void showAlert(BuildContext context) {
+    Widget resumeButton = FlatButton(
+      child: Text("Resume"),
+      onPressed: () {
+        alerted = false;
+        Navigator.pop(context);
+      },
+    );
+
+    showDialog(
+        context: context,
+        barrierDismissible: false,
+        builder: (context) => AlertDialog(
+          title: Text("ALERT"),
+          content: Text("I've fallen down. Help me!"),
+          actions: [
+            resumeButton
+          ],
+        )
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
